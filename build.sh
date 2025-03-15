c3387201b90aae766c5d8e363b976bb0e57d351a
2lar <lawrencelkim@gmail.com>
2025-03-14 23:28:57 -0700

    remove fc thiongs

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# This script will modify the source files to remove TypeScript annotations and then build
+# This script will build the project without manually stripping TypeScript annotations
 
 # Install dependencies
 npm install
@@ -8,21 +8,9 @@ npm install
 npm run build:shared
 npm run build:server
 
-# Fix client TypeScript issues by removing annotations
+# Build client code without modifying TypeScript annotations
 cd packages/client
-echo "Fixing TypeScript annotations in client code..."
-
-# 1. Replace "React.FC" with regular function syntax in App.tsx
-sed -i 's/const App: React\.FC/const App/g' src/App.tsx
-
-# 2. Replace other React.FC instances in any component
-find src -name "*.tsx" -type f -exec sed -i 's/: React\.FC[<][^>]*[>]//g' {} \;
-find src -name "*.tsx" -type f -exec sed -i 's/: React\.FC//g' {} \;
-
-# 3. Remove other TypeScript annotations
-find src -name "*.tsx" -type f -exec sed -i 's/: \(string\|number\|boolean\|any\|void\)\(\[\]\)\?//g' {} \;
-
-# 4. Attempt to build with TypeScript errors ignored
+echo "Building client code..."
 GENERATE_SOURCEMAP=false CI=false npm run build
 
 cd ../..
